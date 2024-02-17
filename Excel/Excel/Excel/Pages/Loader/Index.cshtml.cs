b7d79a7f748463fdfd056f90fc4a76e3a47ed4ee
nexto1993 <nexto.conan@gmail.com>
2024-02-17 21:06:22 +0300

    loader

---
+++ Excel/Excel/Excel/Pages/Loader/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Excel.Pages.Loader
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
