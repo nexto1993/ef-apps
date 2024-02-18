e77b42b8ad317f29e7e9e8b42d89cc12d4581157
nexto1993 <nexto.conan@gmail.com>
2024-02-18 20:33:55 +0300

    loader 3

--- Excel/Excel/Excel/Pages/Loader/Index.cshtml.cs
+++ Excel/Excel/Excel/Pages/Loader/Index.cshtml.cs
@@ -5,8 +5,18 @@ namespace Excel.Pages.Loader
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public string EntriesUrl { get; set; }
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            var entriesUrl = _configuration.GetValue<string>("ApiSettings:EntriesUrl");
+            // Pass the URL to the view
+            EntriesUrl = entriesUrl;
         }
     }
 }
